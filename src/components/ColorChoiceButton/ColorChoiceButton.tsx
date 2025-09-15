d986e97946bedd65131020c50c716cb6cd4f937d
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-15 11:48:05 +0200

    created basic button component with color options

---
+++ src/components/ColorChoiceButton/ColorChoiceButton.tsx
@@ -0,0 +1,7 @@
+import style from "./ColorChoiceButton.module.css";
+
+type ColorChoiceButtonProps = {};
+
+export default function ColorChoiceButton({}: ColorChoiceButtonProps) {
+  return <button className={style.button}></button>;
+}
