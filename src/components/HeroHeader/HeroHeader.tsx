4f3e066f7f4527919d2e5b26b44dd9b3e2783bb2
Andreawingardh <andreawingardh@gmail.com>
2025-09-16 09:25:52 +0200

    Add HeroHeader component with styles
    Introduced the HeroHeader React component with associated CSS module for layout and typography. The component displays a styled heading and description, providing a visually appealing header section.

---
+++ src/components/HeroHeader/HeroHeader.tsx
@@ -0,0 +1,12 @@
+import styles from './HeroHeader.module.css'
+
+export function HeroHeader() {
+    return (
+        <div className={styles.heroHeaderContainer}>
+            <h1>Design Your Card</h1>
+            <p>Create a credit card that reflects your style. Customize colors, patterns and
+personalization to make it uniquely yours </p>
+        </div>
+
+    )
+}
