6501462dc89e0133ee59985c54458616316a3eb2
Kasper Futyna <kasfutable@gmail.com>
2025-09-17 13:06:13 +0200

    Add: Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerNavContainer}>
+        <div className={styles.headerLogo}>
+          <img src="public/assets/images/CardHouse_logo.svg"></img>
+          <h3>Card House</h3>
+        </div>
+        <div className={styles.headerNav}>
+          <p>Cards</p>
+          <p>Rewards</p>
+          <p>Support</p>
+        </div>
+      </div>
+      <div className={styles.headerBorder}></div>
+    </div>
+  );
+}
