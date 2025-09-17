ec722e5161514f9cca2ce94ff4b6d256099c52bb
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-17 10:24:16 +0200

    added color selection toggle

--- src/components/ColorChoiceButtons/ColorChoiceButtons.tsx
+++ src/components/ColorChoiceButtons/ColorChoiceButtons.tsx
@@ -1,3 +1,28 @@
 import styles from "./ColorChoiceButtons.module.css";
+import { useState } from "react";
+import ColorChoiceButton from "../ColorChoiceButton/ColorChoiceButton";
 
-export default function ColorChoiceButtons() {}
+const colors = ["paleBlue", "blue", "green", "orange", "red", "metal"] as const;
+type ButtonColor = (typeof colors)[number];
+
+export default function ColorChoiceButtons() {
+  const [selectedColor, setSelectedColor] = useState<ButtonColor | null>(null);
+
+  return (
+    <div className={styles.container}>
+      {colors.map((color) => (
+        <div
+          key={color}
+          className={`${styles.buttonWrapper} ${
+            selectedColor === color ? styles.selected : ""
+          }`}
+        >
+          <ColorChoiceButton
+            color={color}
+            onClick={() => setSelectedColor(color)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
