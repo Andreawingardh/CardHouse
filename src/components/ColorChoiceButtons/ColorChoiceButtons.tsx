cfc476fcf9da0518c46c163f4785d9052cd9ac60
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-17 11:01:47 +0200

    changed mock overlay to designed overlay

--- src/components/ColorChoiceButtons/ColorChoiceButtons.tsx
+++ src/components/ColorChoiceButtons/ColorChoiceButtons.tsx
@@ -13,14 +13,67 @@ export default function ColorChoiceButtons() {
       {colors.map((color) => (
         <div
           key={color}
-          className={`${styles.buttonWrapper} ${
-            selectedColor === color ? styles.selected : ""
-          }`}
+          className={styles.buttonWrapper}
+          onClick={() => setSelectedColor(color)}
         >
-          <ColorChoiceButton
-            color={color}
-            onClick={() => setSelectedColor(color)}
-          />
+          <ColorChoiceButton color={color} />
+          {selectedColor === color && (
+            <div className={styles.overlay}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="57"
+                height="58"
+                viewBox="0 0 57 58"
+                fill="none"
+              >
+                <g filter="url(#filter0_f_15_27)">
+                  <rect
+                    x="6"
+                    y="6"
+                    width="45"
+                    height="46"
+                    rx="8"
+                    stroke="white"
+                    strokeWidth="3"
+                  />
+                  <circle cx="28" cy="29" r="5" fill="white" />
+                </g>
+                <rect
+                  x="6"
+                  y="6"
+                  width="45"
+                  height="46"
+                  rx="8"
+                  stroke="white"
+                  strokeWidth="3"
+                />
+                <circle cx="28" cy="29" r="5" fill="white" />
+                <defs>
+                  <filter
+                    id="filter0_f_15_27"
+                    x="0.5"
+                    y="0.5"
+                    width="56"
+                    height="57"
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                  >
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="BackgroundImageFix"
+                      result="shape"
+                    />
+                    <feGaussianBlur
+                      stdDeviation="2"
+                      result="effect1_foregroundBlur_15_27"
+                    />
+                  </filter>
+                </defs>
+              </svg>
+            </div>
+          )}
         </div>
       ))}
     </div>
