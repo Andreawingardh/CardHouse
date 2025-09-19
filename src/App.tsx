b5a6d52c345b4bf1a07a99eb8e2e13cc321a1a9a
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-19 12:29:35 +0200

    added customization panel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import HeroHeader from "./components/HeroHeader/HeroHeader";
+import CustomizationPanel from "./components/CustomizationPanel/CustomizationPanel";
 
 function App() {
   return (
@@ -9,7 +10,12 @@ function App() {
       <section className="hero">
         <HeroHeader />
       </section>
-      <section className="customization"></section>
+      <section className="customization">
+        <div className="cardModelStandIn">
+          {/* Stand in div to replace with Card Model once it is done */}
+        </div>
+        <CustomizationPanel />
+      </section>
     </>
   );
 }
