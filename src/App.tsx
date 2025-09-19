38dee6ddbb94db6c0f62a593cf93552501f8b747
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-19 12:22:15 +0200

    imported header and hero header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import HeroHeader from "./components/HeroHeader/HeroHeader";
 
 function App() {
   return (
     <>
-      <section className="hero"></section>
+      <Header />
+      <section className="hero">
+        <HeroHeader />
+      </section>
       <section className="customization"></section>
     </>
   );
