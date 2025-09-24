265149314475650cd8b60f7ac96f95a084d06fd8
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-24 10:12:47 +0200

    added opacity to shapes to let them match background color

--- src/components/CardModel/CardModelScene.tsx
+++ src/components/CardModel/CardModelScene.tsx
@@ -154,17 +154,21 @@ export function CardModelScene() {
           new THREE.PlaneGeometry(size.x, size.z),
           textureMaterial
         );
+
         // Function to create pattern textures as SVG
         function createPatternTexture(pattern: string): THREE.Texture {
           let svgString = "";
+          const opacity = 0.4; // Adjust opacity between 0 and 1
+          const stroke = `rgba(255,255,255,${opacity})`;
+          const fill = `rgba(255,255,255,${opacity})`;
 
           switch (pattern) {
             case "squares":
               svgString = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 39" width="512" height="320">
-          <rect x="35.5" y="5.7" width="11" height="11" transform="rotate(45 35.5 5.7)" stroke="#737C89" fill="none"/>
-          <rect x="10.3" y="18.7" width="6.4" height="6.4" transform="rotate(24.5 10.3 18.7)" stroke="#737C89" fill="none"/>
-          <rect x="20.6" y="29.1" width="4.1" height="4.1" transform="rotate(-20.3 23.6 29.1)" stroke="#737C89" fill="none"/>
+          <rect x="35.5" y="5.7" width="11" height="11" transform="rotate(45 35.5 5.7)" stroke="${stroke}" fill="none"/>
+          <rect x="10.3" y="18.7" width="6.4" height="6.4" transform="rotate(24.5 10.3 18.7)" stroke="${stroke}" fill="none"/>
+          <rect x="20.6" y="29.1" width="4.1" height="4.1" transform="rotate(-20.3 23.6 29.1)" stroke="${stroke}" fill="none"/>
         </svg>
       `;
               break;
@@ -172,9 +176,9 @@ export function CardModelScene() {
             case "stripes":
               svgString = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 39" width="512" height="320">
-          <rect x="2" y="13" width="45" height="2" fill="#6B7584" />
-          <rect x="2" y="18" width="34" height="2" fill="#6B7584" />
-          <rect x="2" y="23" width="24" height="2" fill="#6B7584" />
+          <rect x="2" y="13" width="45" height="2" fill="${fill}" />
+          <rect x="2" y="18" width="34" height="2" fill="${fill}" />
+          <rect x="2" y="23" width="24" height="2" fill="${fill}" />
         </svg>
       `;
               break;
@@ -182,13 +186,13 @@ export function CardModelScene() {
             case "circles":
               svgString = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49 39" width="512" height="320">
-          <circle cx="7.5" cy="31.5" r="2.5" fill="#737C8A" />
-          <circle cx="41.5" cy="6.5" r="2.5" fill="#737C8A" />
-          <circle cx="10" cy="24" r="2" fill="#737C8A" />
-          <circle cx="33" cy="8" r="2" fill="#737C8A" />
-          <circle cx="42" cy="14" r="2" fill="#737C8A" />
-          <circle cx="16.5" cy="26.5" r="1.5" fill="#737C8A" />
-          <circle cx="36.5" cy="17.5" r="1.5" fill="#737C8A" />
+          <circle cx="7.5" cy="31.5" r="2.5" fill="${fill}" />
+          <circle cx="41.5" cy="6.5" r="2.5" fill="${fill}" />
+          <circle cx="10" cy="24" r="2" fill="${fill}" />
+          <circle cx="33" cy="8" r="2" fill="${fill}" />
+          <circle cx="42" cy="14" r="2" fill="${fill}" />
+          <circle cx="16.5" cy="26.5" r="1.5" fill="${fill}" />
+          <circle cx="36.5" cy="17.5" r="1.5" fill="${fill}" />
         </svg>
       `;
               break;
