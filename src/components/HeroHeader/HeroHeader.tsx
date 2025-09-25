6d725dd6e296d62a4d69b9d0b03d3704f60f1257
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-09-25 12:51:47 +0200

    reverted back to div

--- src/components/HeroHeader/HeroHeader.tsx
+++ src/components/HeroHeader/HeroHeader.tsx
@@ -2,12 +2,12 @@ import styles from "./HeroHeader.module.css";
 
 export default function HeroHeader() {
   return (
-    <section className={styles.heroHeaderContainer}>
+    <div className={styles.heroHeaderContainer}>
       <h1>Design Your Card</h1>
       <p>
         Create a credit card that reflects your style. Customize colors,
         patterns and personalization to make it uniquely yours{" "}
       </p>
-    </section>
+    </div>
   );
 }
